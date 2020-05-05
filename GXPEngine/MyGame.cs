caa3bd2aa525b301a2f80f9a71ead5ff7523ae3a
Pepijn Wasser <482906@student.saxion.nl>
2020-05-05 14:25:59 +0200

    Changed full screen

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -5,7 +5,7 @@ using GXPEngine;								// GXPEngine contains the engine
 public class MyGame : Game
 {
 	//----------------change size to correct screen size
-	public MyGame() : base(1920, 1080, true)		
+	public MyGame() : base(1920, 1080, false)		
 	{		
 		Screens screen = new Screens();
 		AddChild(screen);	
