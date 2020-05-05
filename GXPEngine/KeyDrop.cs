c36aefe21239e769c85665cb2c474f82229c2ebe
peppipeps <60891244+peppipeps@users.noreply.github.com>
2020-05-05 13:22:43 +0200

    Add files via upload

---
+++ GXPEngine/KeyDrop.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class KeyDrop : Sprite
+    {
+        public string buttonToPress;
+
+        public KeyDrop() : base("square.png")
+        {
+            SetPosition();
+            SetButton();
+        }
+
+        void SetPosition()
+        {
+            int column = Utils.Random(1, 5);
+            if (column == 1)
+            {
+                this.SetXY(1250, 0);
+            }
+            if (column == 2)
+            {
+                this.SetXY(1325, 0);
+            }
+            if (column == 3)
+            {
+                this.SetXY(1400, 0);
+            }
+            if (column == 4)
+            {
+                this.SetXY(1475, 0);
+            }
+        }
+
+        void SetButton()
+        {
+            int button = Utils.Random(1, 3);
+
+            if(button == 1)
+            {
+                buttonToPress = "A";
+            }
+            if(button == 2)
+            {
+                buttonToPress = "B";
+            }
+        }
+    }
+}
---
+++ GXPEngine/KeyDropControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class KeyDropControl : Canvas
+    {
+        List<KeyDrop> _keyDrops;
+        KeyDrop _keyDrop;
+
+        public int GetNumberOfKeyDrops()
+        {
+            return _keyDrops.Count;
+        }
+
+
+        public KeyDropControl() : base (1920, 1080)
+        {
+      
+        }
+
+        void Update()
+        {
+            AddKeyDrop();
+        }
+
+        void AddKeyDrop()
+        {
+            _keyDrop = new KeyDrop();
+            AddChild(_keyDrop);
+            _keyDrops.Add(_keyDrop);
+        }
+    }
+}
