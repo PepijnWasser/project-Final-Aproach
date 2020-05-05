ffef2f01017f966cc14945051d85a5f1f19b13d8
peppipeps <60891244+peppipeps@users.noreply.github.com>
2020-05-05 10:29:03 +0200

    Add files via upload

---
+++ GXPEngine/Background.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class Background :AnimationSprite
+    {
+        public Background() : base("background.png", 4, 1)
+        {
+            this.SetXY(50, -15);
+            this.SetScaleXY((float)1.9, (float)1.9);
+        }
+
+        //changes background color
+        public void UpdateFrame()
+        {
+                NextFrame();
+        }
+    }
+}
