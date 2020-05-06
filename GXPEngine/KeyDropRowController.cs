e786db736b3e21c448bcc617937b48e5f2660d93
peppipeps <60891244+peppipeps@users.noreply.github.com>
2020-05-06 14:19:37 +0200

    Add files via upload

---
+++ GXPEngine/KeyDropRowController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class KeyDropRowController : Sprite
+    {
+        public int row = 1;
+
+        public KeyDropRowController() : base("square.png")
+        {
+            this.SetXY(1250, 0);
+        }
+
+        void Update()
+        {
+            CheckRow();
+            UpdateRow();
+        }
+
+        void CheckRow()
+        {
+            if (Input.GetKeyDown(Key.Q))
+            {
+                row = row - 1;
+            }
+            if (Input.GetKeyDown(Key.E))
+            {
+                row = row + 1;
+            }
+
+            if(row > 4)
+            {
+                row = 1;
+            }
+            if(row < 1)
+            {
+                row = 4;
+            }
+        }
+
+        void UpdateRow()
+        {
+            if(row == 1)
+            {
+                this.SetXY(1250, 0);
+            }
+            if (row == 2)
+            {
+                this.SetXY(1325, 0);
+            }
+            if (row == 3)
+            {
+                this.SetXY(1400, 0);
+            }
+            if (row == 4)
+            {
+                this.SetXY(1475, 0);
+            }
+        }
+    }
+}
